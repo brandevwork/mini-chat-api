e07bc3d7093d9f50777cc5864e78071532fd89ec
silversoftdev <kennethberthelotwork@gmail.com>
2022-11-17 10:11:52 +0100

    Connect knex

---
+++ data/dbConfig.js
@@ -0,0 +1,9 @@
+import knex from 'knex'
+// const knex = require('knex');
+
+import knexConfig from '../knexfile.js'
+// const knexConfig = require('../knexfile.js');
+
+const dbEnv = process.env.DB_ENV || 'development';
+
+module.exports = knex(knexConfig[dbEnv]);
