eba34a8383880f6e9bfae4d014d8489af79ed28a
silversoftdev <kennethberthelotwork@gmail.com>
2022-11-21 14:12:17 +0100

    Rename index file

---
+++ index.ts
@@ -0,0 +1,14 @@
+// import dotenv from 'dotenv'
+const dotenv = require("dotenv");
+dotenv.config();
+
+const server = require('./server');
+// import server from "./server.js";
+
+// const process: NodeJS.Process;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 5050;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on port ${port}***\n`);
+});
