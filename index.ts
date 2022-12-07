638f78e8c89626f015920cd731d3e415e434d250
silversoftdev <kennethberthelotwork@gmail.com>
2022-12-07 22:14:58 +0100

    Clean up file

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-// import dotenv from 'dotenv'
 const dotenv = require("dotenv");
 dotenv.config();
 
 const server = require('./server');
-// import server from "./server.js";
 
-// const process: NodeJS.Process;
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 5050;
 
