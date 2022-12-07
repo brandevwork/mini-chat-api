4899911d8a8371fca50f29d9344371fc1abadf2f
silversoftdev <kennethberthelotwork@gmail.com>
2022-12-07 22:16:40 +0100

    Add types and clean up user service

--- routes/users/userDB.ts
+++ routes/users/userDB.ts
@@ -1,3 +1,5 @@
+import { IUser } from "../../interfaces/users";
+
 export {};
 const db = require("../../data/dbConfig.js");
 
@@ -17,11 +19,11 @@ function getBy(filter: any) {
   return db("users").where(filter);
 }
 
-function getById(id: any) {
+function getById(id: number) {
   return db("users").select("id", "username", "email").where({ id }).first();
 }
 
-function create(user: any) {
+function create(user: IUser) {
   return db("users")
     .insert(user)
     .then((id: any) => {
@@ -29,15 +31,15 @@ function create(user: any) {
     });
 }
 
-function update(id: any, changes: any) {
+function update(id: number, changes: IUser) {
   return db("users")
     .where({ id })
     .update(changes)
-    .then((id: any) => {
+    .then((id: number) => {
       return getById(id);
     });
 }
 
-function remove(id: any) {
+function remove(id: number) {
   return db("users").where("id", id).del();
 }
--- routes/users/userRouter.ts
+++ routes/users/userRouter.ts
@@ -1,8 +1,7 @@
 export {};
-// import express from 'express';
-// import bcrypt from 'bcrypt';
+import { IUser } from "../../interfaces/users";
+import { Request, Response, NextFunction } from 'express';
 
-// import dbUser from './userDB';
 const express = require("express");
 const bcrypt = require("bcrypt");
 
@@ -15,38 +14,39 @@ userRouter.get("/:id", validateUserId, getUserById);
 userRouter.delete("/:id", validateUserId, deleteUser);
 userRouter.put("/:id", validateUserId, editUser);
 
-function editUser(req: any, res: any) {
+function editUser(req: any, res: any)  {
+
   dbUser
     .update(req.user.id, req.body)
     .then(() => {
       res.status(200).json({ ...req.user, ...req.body });
     })
-    .catch((error: any) => {
+    .catch((error: Error) => {
       res.status(500).json({
         message: "Could not Update User" + error,
       });
     });
 }
 
-function getAllUsers(req: any, res: any) {
+function getAllUsers(req: Request, res: Response)  {
   dbUser
     .get()
-    .then((user: any) => {
+    .then((user: IUser) => {
       res.status(200).json(user);
     })
-    .catch((error: any) => {
+    .catch((error: Error) => {
       res.status(500).json({
         message: "Error adding new user" + error.message,
       });
     });
 }
 
-function getUserById(req: any, res: any) {
+function getUserById(req: any, res: Response)  {
   res.json(req.user);
 }
 
 
-function deleteUser(req: any, res: any) {
+function deleteUser(req: any, res: Response) {
   dbUser.remove(req.user.id).then(() => {
     res
       .status(200)
@@ -55,12 +55,11 @@ function deleteUser(req: any, res: any) {
 }
 
 // Middleware
-
-function validateUserId(req: any, res: any, next: any) {
+function validateUserId(req: any, res: Response, next: NextFunction) {
   const id = req.params.id;
   dbUser
     .getById(id)
-    .then((user: any) => {
+    .then((user: IUser) => {
       if (user) {
         req.user = user;
         next();
@@ -70,7 +69,7 @@ function validateUserId(req: any, res: any, next: any) {
         });
       }
     })
-    .catch((error: any) => {
+    .catch((error: Error) => {
       res.status(404).json({ message: " Invalid user id:" + error.message });
     });
 }
