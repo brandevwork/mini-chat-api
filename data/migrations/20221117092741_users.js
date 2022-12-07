bd05f110f75b6b86a6bc64de6b9f194fa4afb132
silversoftdev <kennethberthelotwork@gmail.com>
2022-12-07 20:20:15 +0100

    Update users table migration

--- data/migrations/20221117092741_users.js
+++ data/migrations/20221117092741_users.js
@@ -2,22 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const up = function(knex) {
-  return knex.schema.createTable('users', (users) => {
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (users) => {
     users.increments();
-    users.string('username').notNullable().unique();
-    users.string('email').notNullable().unique();
-    users.string('password').notNullable();
-  })
+    users.string("username").notNullable().unique();
+    users.string("email").notNullable().unique();
+    users.string("password").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const down = function(knex) {
-  return knex.schema.dropTableIfExists('users')
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
 };
-
-export { up, down }
- 
