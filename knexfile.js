02032e29c33d4567883ac852dac1ccfafb6ed3dc
silversoftdev <kennethberthelotwork@gmail.com>
2022-12-15 10:04:43 +0100

    Update db connection

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 // Update with your config settings.
 
 /**
@@ -5,29 +7,31 @@
  */
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: "pg",
     connection: {
-      filename: "./data/users.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
+      host : process.env.DATABASE_URL, // should be remote host
+      user : 'root',
+      password : 'your_database_password',
+      database : 'chatappdb'
     },
     migrations: {
       directory: "./data/migrations",
       tableName: "knex_migrations",
     },
-    seeds: {
-      directory: "./data/seeds",
-    },
+    seeds: { directory: "./data/seeds" }
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./database/migrations"
+    connection: { // MOVE TO ENV. VARS
+      host : 'batyr.db.elephantsql.com', // should be remote host
+      user : 'kgdgfacj',
+      password : 'tcBjZGa0R6DM_3S57_hvrJ_Pa1uwbrGm',
+      database : 'kgdgfacj'
     },
-    seeds: { directory: "./database/seeds" }
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: { directory: "./data/seeds" }
   }
 };
